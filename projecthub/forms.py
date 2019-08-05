bfc1bc4a1af8bd126c887a3853cdc40ce5302a20
muhindokiro <blaize1143@gmail.com>
2019-08-05 11:53:26 +0300

    forms.py, import django forms and project class

--- projecthub/forms.py
+++ projecthub/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Project
 
+
 class NewsLetterForm(forms.Form):
     your_name = forms.CharField(label='First Name',max_length=30)
     email = forms.EmailField(label='Email')
