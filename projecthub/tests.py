b87343dcd7a6a56920f6749abdf7af756514fdec
muhindokiro <blaize1143@gmail.com>
2019-08-05 21:27:00 +0300

    add project, profile and review tests in tests.py file

--- projecthub/tests.py
+++ projecthub/tests.py
@@ -1,4 +1,31 @@
 from django.test import TestCase
-from .models import Project,Review
+from .models import Project,Review,Profile
 
 # Create tests here.
+class ProjectTestClass(TestCase):
+
+    def setUp(self):
+        self.new_project= Project(title = 'Test project',project = 'This is a random test Project')
+        self.new_project.save()
+
+    def tearDown(self):
+        Project.objects.all().delete()
+
+class ReviewTestClass(TestCase):
+
+    def setUp(self):
+        self.new_review= Review(title = 'Test review',review = 'This is a random test Review')
+        self.new_review.save()
+
+    def tearDown(self):
+        Review.objects.all().delete()
+
+class ProfileTestClass(TestCase):
+
+    def setUp(self):
+        self.new_profile= Profile(title = 'Test profile',profile = 'This is a random test Profile')
+        self.new_profile.save()
+
+
+    def tearDown(self):
+        Profile.objects.all().delete()
